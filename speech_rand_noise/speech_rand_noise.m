f69199c8111066a23025b75ba4f5ec1075ed5828
Dante Smith <djsmith@bu.edu>
2018-04-24 01:46:00 -0400

    A bit more cleanup on the SSN creation script

--- speech_rand_noise/speech_rand_noise.m
+++ speech_rand_noise/speech_rand_noise.m
@@ -8,52 +8,60 @@
 %% output is then converted back into the time domain using an inverse
 %% fourier transform. The resultant is a speech shaped noise with spectrum
 %% almost identical to that of the original speech corpus
+clear
+[files, path] = uigetfile([pwd '\*.wav'], 'Select the speech files', 'MultiSelect', 'on'); % Selectin all the wave files to be included in the analysis
 
-[files,path] = uigetfile([pwd '\*.wav'], 'Select the speech files', 'MultiSelect', 'on'); % Selectin all the wave files to be included in the analysis
-
-[row,col] = size(files); %Determining the number of wave files loaded
-long = []; %preallocating the array to concatenate all the speech files
-for i = 1:col
-    [data,fs] = audioread([path,char(files(i))]);% reading the data file
-    data =0.999*data/max(abs(data)); % Normalizing the amplitudes 
-    if i ==1
+numFiles = length(files); % Determining the number of wave files loaded
+long = [];                % Preallocating the array to concatenate all the speech files
+for i = 1:numFiles
+    filename = fullfile(path, files{i});
+    [data, fs] = audioread(filename); % reading the data file
+    data = 0.999*data/max(abs(data)); % Normalizing the amplitudes 
+    if i == 1
         Fs = fs;
     elseif fs~=Fs 
-    specnoise = resample(data,fs,Fs);
+        specnoise = resample(data,fs,Fs);
     end
     long = [long; data]; % concatenating the speech files
-
 end
 
 spec = abs(fft(long)).*exp(j*2*pi*rand(size(long))); % Applying the fourier transform and randomizing the phases of all the spectral components
 
 noise = real(ifft(spec)); % Obatining the real parts of the IFFT
-long = 0.999*long/max(abs(long)); % Normalizing the concatenated speech (For display purposes only)
+long  = 0.999*long/max(abs(long)); % Normalizing the concatenated speech (For display purposes only)
 noise = 0.999*noise/max(abs(noise)); %Normalizing the speech noise amplitude for wavwrite
 
-audiowrite(noise,fs,'speech_noise_phaserand') % Change the name of the wave file to your liking
-
+audiowrite('SSN.wav', noise, fs) % Change the name of the wave file to your liking
 
 %% Plotting the Spectra of the speech corpus and the generated noise
-
-fft_x=fft(long);
-amp=abs(fft_x);
-n=length(fft_x);
-freq=fs/n.*(1:n);
+fft_x  = fft(long);
+amp    = abs(fft_x);
+n      = length(fft_x);
+freq   = fs/n.*(1:n);
 aspeech=amp(1:(n/2));
-f=freq(1:(n/2));
-plot(f,aspeech,'k');
+f = freq(1:(n/2));
 
-fft_x=fft(noise);
-amp=abs(fft_x);
-n=length(fft_x);
-freq=fs/n.*(1:n);
-anoise=amp(1:(n/2));
-f=freq(1:(n/2));
+fft_x  = fft(noise);
+amp    = abs(fft_x);
+n      = length(fft_x);
+freq   = fs/n.*(1:n);
+anoise = amp(1:(n/2));
+f = freq(1:(n/2));
 
+plotpos = [10 100];
+plotdim = [1600 600];
+
+SSNFig = figure('Color', [1 1 1]);
+set(SSNFig, 'Position', [plotpos plotdim],'PaperPositionMode','auto')
+
+plot(f, aspeech,'k');
 hold on
+plot(f, anoise,'r');
+xlabel('Frequency (Hz)')
+ylabel('Magnitude')
 
-plot(f,anoise,'r');
-
+axis([50 10000 0 15000])
+set(gca, 'XScale', 'log')
+box off
 
 legend('Speech', 'Noise')
